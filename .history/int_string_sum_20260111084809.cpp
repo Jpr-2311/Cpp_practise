50da1ae8e87d64c1eb91aa7716248416f4cc98e3
Jpr-2311 <jithinpr888@gmail.com>
2026-01-11 08:55:20 +0530

    Add initial C++ practice programs

---
+++ .history/int_string_sum_20260111084809.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+    void operate( vector<pair<int, string>> &pairs) {
+        int sum=0;
+        string result="";
+        for(auto p : pairs){
+            sum += p.first; 
+            result+=p.second;
+        }
+        cout << sum<<endl;
+        cout << result<<endl;
+        cout<< result.length() << endl;
+        
+        
+    }
+};
---
+++ .history/int_string_sum_20260111084922.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    void operate(vector<pair<int, string>> &pairs) {
+        int sum = 0;
+        string result = "";
+
+        for (auto p : pairs) {
+            sum += p.first;
+            result += p.second;
+        }
+
+        cout << sum << endl;
+        cout << result << endl;
+        cout << result.length() << endl;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;   // number of pairs
+
+    vector<pair<int, string>> pairs;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        string s;
+        cin >> x >> s;
+        pairs.push_back({x, s});
+    }
+
+    solution obj;
+    obj.operate(pairs);
+
+    return 0;
+}
